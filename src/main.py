18804b90ae179336295ef32554553e2df6428806
rajkumardubey10 <dubeyrajkumar@683gmail.com>
2026-01-02 11:45:07 +0000

    checking for ci demo github action

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ app = FastAPI(title="Simple FastAPI App")
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello FastAPI From Dev 🚀"}
+    return {"message": "Hello FastAPI From fix branch for ci demo 🚀"}
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
 
 @app.get("/items/{item_id}")
-def get_item(item_id: int, q: str | None = None):
+def get_item(item_id: int, q: str |  None = None):
     return {
         "item_id": item_id,
         "query": q
